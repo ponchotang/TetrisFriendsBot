ff081a87efae3b25bcffca365ab6f633da7bc4c8
Andy Tang <andytang247@gmail.com>
2016-12-13 17:13:18 +1300

    Starting implementing AlgorithmO.

--- bot/AlgorithmO.java
+++ bot/AlgorithmO.java
@@ -1,11 +1,34 @@
 package bot;
 
+import java.util.List;
+
+import game.Tile;
+
 public class AlgorithmO extends Algorithm{
+	
+	private static final int DEFAULT_POSITION = 4;
 
 	@Override
 	public String execute() {
 		// TODO Make use of findGap to decide on basic piece placements
-		return determineHorizontalMovement(4, 8);
+		
+		String moves = "";
+		
+		List<Tile> gaps = findGap(2,2);
+		
+		if (gaps.size() > 0) {
+			moves += determineHorizontalMovement(DEFAULT_POSITION, gaps.get(0).column()) + "s";
+		}
+		
+		if (moves.length() == 0) {
+			gaps = findGap(2,1);
+			
+			if (gaps.size() > 0) {
+				moves += determineHorizontalMovement(DEFAULT_POSITION, gaps.get(0).column()) + "s";
+			}
+		}
+		
+		return moves;
 	}
 	
 
