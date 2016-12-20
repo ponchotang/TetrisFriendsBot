a746a9b4aa72f3363e9822e40c38036ad6573118
Andy Tang <andytang247@gmail.com>
2016-12-21 12:58:22 +1300

    Added comments

--- bot/AlgorithmJ.java
+++ bot/AlgorithmJ.java
@@ -27,7 +27,7 @@ public class AlgorithmJ extends Algorithm {
 
 	/**
 	 * This method aims to find a perfect fitting gap for the J tetrimino rotated
-	 * 180 degrees.
+	 * 90 degrees
 	 * 
 	 * It uses the findGap and findSpaces method in order to find the fitting gap.
 	 * 
@@ -35,25 +35,28 @@ public class AlgorithmJ extends Algorithm {
 	 */
 	private List<Tile> findFittingGap() {
 		List<Tile> foundGaps = new ArrayList<Tile>();
-
-		// Get gaps of size 1,2
-		// Get flat spaces of size 1
-		// Iterate through both in a nested loop
-		// Check if the two current gaps and spaces form the fitting gap
-
+		
+		// Get list of gaps and spaces which can combine to make up the J shape
 		List<Tile> gaps = findGap(1,2);
 		List<Tile> spaces = findFlatSpace(1);
 
+		// Iterate through all gaps and spaces
 		for (int i = 0; i < gaps.size(); i++) {
 			for (int j = 0; j < spaces.size(); j++) {
+				
+				// Get both respective pieces
 				Tile bottomPiece = gaps.get(i);
 				Tile topPiece = spaces.get(j);
 				
+				// Check if the pieces are in the correct position
+				// If they are, then they form the fitting gap
 				if (bottomPiece.column() == topPiece.column() - 1 &&
 						bottomPiece.row() - 1 == topPiece.row()) {
 					
+					// Get the top-left corner of this fitting gap
 					Tile intersectedTile = game.get(topPiece.row(), bottomPiece.column());
 					
+					// Add to foundGaps if it is clear
 					if (isClear(intersectedTile, 2)) {
 						foundGaps.add(game.get(topPiece.row(), bottomPiece.column()));
 					}
