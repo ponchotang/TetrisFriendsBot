dfce9c88fafaa6ec547e6b29db9f43414f36c420
Andy Tang <andytang247@gmail.com>
2016-12-29 12:05:51 +1300

    Added fields describing the tetrimino's position

--- AlgorithmSZ.java
+++ AlgorithmSZ.java
@@ -1,4 +1,13 @@
 
 public class AlgorithmSZ {
+	
+	protected static int DEFAULT_VERTICAL_POSITION_HANGING;
+	protected static int DEFAULT_VERTICAL_POSITION_FLAT;
+	protected static int DEFAULT_HORIZONTAL_POSITION = 3;
+	
+	protected static String VERTICAL_HANGING;
+	protected static String HORIZONTAL_HANGING;
+	protected static String VERTICAL_FLAT;
+	protected static String HORIZONTAL_FLAT;
 
 }
