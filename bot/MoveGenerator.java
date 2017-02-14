89ffa9921645d1a245cf728b267184cc1aa1bb86
Andy Tang <andytang247@gmail.com>
2017-02-14 19:31:02 +1300

    Start implementation of MoveGenerator

--- bot/MoveGenerator.java
+++ bot/MoveGenerator.java
@@ -1,9 +1,20 @@
 package bot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import game.Tetrimino;
+
 public class MoveGenerator {
 	/*
 	 * This class has one primary method: generateMoves(Tetrimino tetrimino)
 	 * The goal of this is to return a list of Strings where each String describes one
 	 * of the possible moves for the current tetrimino.
 	 */
+	
+	public List<String> generateMoves(Tetrimino tetrimino) {
+		List<String> moves = new ArrayList<String>();
+		
+		return moves;
+	}
 }
