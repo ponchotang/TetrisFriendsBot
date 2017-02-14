ced11c941a5306c389b35b4c5a501e082ba492c3
Andy Tang <andytang247@gmail.com>
2017-02-14 19:18:21 +1300

    added rough outline of SimulatedClass implementation

--- bot/SimulatedGame.java
+++ bot/SimulatedGame.java
@@ -1,5 +1,16 @@
 package bot;
 
 public class SimulatedGame {
-
+	/*
+	 * Allows creation of a simulation by using the current state of the real game
+	 * This allows us to test all possible moves and retrieve game states from them in order
+	 * to determine which is the best move
+	 * 
+	 * This class will be created by using TetrisGame as a parameter.
+	 * It will recreate the board state
+	 * 
+	 * It will have a simulate move method which takes in a tetrimino and an input of moves.
+	 * 
+	 * There will also be a score method which calculates the score of the current state
+	 */
 }
