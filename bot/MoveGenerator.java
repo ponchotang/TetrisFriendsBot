ddc2a0f9f5c09bddeca3b246d2e99a3a9a0c9918
Andy Tang <andytang247@gmail.com>
2017-02-14 19:30:09 +1300

    Add class details

--- bot/MoveGenerator.java
+++ bot/MoveGenerator.java
@@ -1,5 +1,9 @@
 package bot;
 
 public class MoveGenerator {
-
+	/*
+	 * This class has one primary method: generateMoves(Tetrimino tetrimino)
+	 * The goal of this is to return a list of Strings where each String describes one
+	 * of the possible moves for the current tetrimino.
+	 */
 }
