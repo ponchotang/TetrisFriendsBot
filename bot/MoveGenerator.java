e1f8f213e521b9fe5f47ee5da3fe98b1b78cb4d1
Andy Tang <andytang247@gmail.com>
2017-02-15 20:00:07 +1300

    Fully implemented move generation

--- bot/MoveGenerator.java
+++ bot/MoveGenerator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import game.Tetrimino;
+import game.TetrisGame;
 
 public class MoveGenerator {
 	/*
@@ -17,10 +18,32 @@ public class MoveGenerator {
 		
 		String rotationString = "";
 		
-		for(int i = 0; i < tetrimino.orientations(); i++) {
+		for (int i = 0; i < tetrimino.orientations(); i++) {
 			moves.add(rotationString);
+			
+			// calculate possible left and right movements
+			// iterate through them and add to list
+			
+			int leftMovements = tetrimino.startingPosition(i);
+			int rightMovements = TetrisGame.getInstance().width() - tetrimino.startingPosition(i) - tetrimino.width(i);
+			
+			String leftString = "";
+			String rightString = "";
+			
+			for (int j = 0; j < leftMovements; j++) {
+				leftString += 'l';
+				moves.add(rotationString + leftString);
+			}
+			
+			for (int j = 0; j < rightMovements; j++) {
+				rightString += 'r';
+				moves.add(rotationString + rightString);
+			}
+			
 			rotationString += 'c';
 		}
+		
+		
 		
 		return moves;
 	}
