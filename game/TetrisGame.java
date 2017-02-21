6301533ec6d832b3805476e18b437c7c27357ba2
Andy Tang <andytang247@gmail.com>
2017-02-21 19:55:47 +1300

    Moved calculation of height to TetrisGame

--- game/TetrisGame.java
+++ game/TetrisGame.java
@@ -3,10 +3,10 @@ package game;
 public class TetrisGame {
 	protected static int HEIGHT = 20;
 	protected static int WIDTH = 10;
-	
+
 	protected Tetrimino currentTetrimino;
 	protected Tile tiles[][];
-	
+
 	/**
 	 * Used to retrieve the tile in the specified row and column
 	 * @param row row of the tile
@@ -16,43 +16,69 @@ public class TetrisGame {
 	public Tile get(int row, int column) {
 		return tiles[row][column];
 	}
-	
+
+	public int currentHeight() {
+		int height = height();
+
+		for (int i = 0; i < height(); i++) {
+
+			boolean rowEmpty = true;
+
+			for (int j = 0; j < width(); j++) {
+				if (tiles[i][j].filled()) {
+					rowEmpty = false;
+					break;
+				}
+			}
+
+			if (rowEmpty) {
+				height--;
+			}
+
+			else {
+				break;
+			}
+		}
+		
+		return height;
+	}
+
 	/**
 	 * @return Height of the game board
 	 */
 	public int height() {
 		return HEIGHT;
 	}
-	
+
 	/**
 	 * @return Width of the game board
 	 */
 	public int width() {
 		return WIDTH;
 	}
-	
+
 	public Tetrimino getCurrentTetrimino() {
 		return currentTetrimino;
 	}
-	
+
 	/**
 	 * Returns a string representation of the play field
 	 */
 	public String toString() {
 		String stringRepresentation = "";
-		
+
 		// Iterate through all tiles
 		for (int i = 0; i < tiles.length; i++) {
-			
+
 			String row = "";
-			
+
 			for (int j = 0; j < tiles[0].length; j++) {
 				row += tiles[i][j].toString(); // append string representation of tile to the row
 			}
-			
+
 			stringRepresentation += row + "\n"; // append row to the overall string
 		}
-		
+
 		return stringRepresentation;
 	}
 
