be9b71996064710940289d4e7a454fb8ecb72103
Andy Tang <andytang247@gmail.com>
2017-02-21 19:59:51 +1300

    Remove unused imports

--- main/Main.java
+++ main/Main.java
@@ -2,25 +2,12 @@ package main;
 
 import java.awt.AWTException;
 import java.awt.Robot;
-import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import bot.MoveGenerator;
 import bot.SimulatedGame;
-import bot_old.Algorithm;
-import bot_old.AlgorithmI;
-import bot_old.AlgorithmJ;
-import bot_old.AlgorithmL;
-import bot_old.AlgorithmO;
-import bot_old.AlgorithmS;
-import bot_old.AlgorithmT;
-import bot_old.AlgorithmZ;
 import bot_old.Inputer;
-import game.BoardDetector;
 import game.Tetrimino;
-import game.Tile;
 import game.RealGame;
 
 public class Main {
