06ab3d58be91527a344be45517e20d298f30ba99
Andy Tang <andytang247@gmail.com>
2017-02-23 20:01:32 +1300

    Add way to determine nextTetrimino in the update method

--- game/RealGame.java
+++ game/RealGame.java
@@ -55,6 +55,7 @@ public class RealGame extends TetrisGame{
 			}
 			
 			currentTetrimino = bd.getCurrentTetrimino(); // BoardDetector to determine the current tetrimino
+			nextTetrimino = bd.getNextTetrimino();
 		}
 	}
 	
