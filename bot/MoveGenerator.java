9501b67843179d513abe994d361c4a7e5cf47f9c
Andy Tang <andytang247@gmail.com>
2017-02-27 12:34:23 +1300

    Clean up code. Add comments

--- bot/MoveGenerator.java
+++ bot/MoveGenerator.java
@@ -6,30 +6,32 @@ import java.util.List;
 import game.Tetrimino;
 import game.RealGame;
 
+/**
+ * This class is used to generate a list of all the possible moves
+ * for the inputed Tetrimino.
+ * 
+ * @author Andy Tang
+ *
+ */
 public class MoveGenerator {
-	/*
-	 * This class has one primary method: generateMoves(Tetrimino tetrimino)
-	 * The goal of this is to return a list of Strings where each String describes one
-	 * of the possible moves for the current tetrimino.
-	 */
 	
 	public List<String> generateMoves(Tetrimino tetrimino) {
 		List<String> moves = new ArrayList<String>();
 		
 		String rotationString = "";
 		
+		// Iterates through all orientations of the tetrimino
 		for (int i = 0; i < tetrimino.orientations(); i++) {
 			moves.add(rotationString);
 			
-			// calculate possible left and right movements
-			// iterate through them and add to list
-			
+			// Calculates the amount of possible left and right movements for the current orientation
 			int leftMovements = tetrimino.startingPosition(i);
 			int rightMovements = RealGame.getInstance().width() - tetrimino.startingPosition(i) - tetrimino.width(i);
 			
 			String leftString = "";
 			String rightString = "";
 			
+			// Iterate through amount of movements and adds them to a string as well as appending it to the list
 			for (int j = 0; j < leftMovements; j++) {
 				leftString += 'l';
 				moves.add(rotationString + leftString);
@@ -40,6 +42,7 @@ public class MoveGenerator {
 				moves.add(rotationString + rightString);
 			}
 			
+			// Increment rotation string to prepare for the next orientation
 			rotationString += 'c';
 		}
 		
