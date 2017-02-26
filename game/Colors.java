691255661316220d6d3ca8e09bf9232e1384fb3b
Andy Tang <andytang247@gmail.com>
2017-02-27 12:46:56 +1300

    Remove unused fields

--- game/Colors.java
+++ game/Colors.java
@@ -3,8 +3,8 @@ package game;
 import java.awt.Color;
 
 /**
- * Contains the different colors
- * @author Andy
+ * Contains the different colors to be used for game detection
+ * @author Andy Tang
  *
  */
 public class Colors {
@@ -13,12 +13,4 @@ public class Colors {
 	public static final Color EMPTY_TILE_2 = new Color(47, 47, 47);
 	public static final Color TETRIMINO_HIGHLIGHT_1 = new Color(79, 79, 79);
 	public static final Color TETRIMINO_HIGHLIGHT_2 = new Color(80, 80, 80);
-	
-	public static final Color TETROMINO_I = new Color(15, 155, 215); // LIGHT BLUE
-	public static final Color TETROMINO_O = new Color(227, 159, 2); // YELLOW
-	public static final Color TETROMINO_T = new Color(175, 41, 138); // PURPLE
-	public static final Color TETROMINO_S = new Color(89, 177, 1); // GREEN
-	public static final Color TETROMINO_Z = new Color(215, 15, 55); // RED
-	public static final Color TETROMINO_J = new Color(33, 65, 198); // BLUE
-	public static final Color TETROMINO_L = new Color(227, 91, 2); // ORANGE
 }
